3b82e98a8ec45be0293fb650790091bc268352a7
Shubham074-ux <127836322+Shubham074-ux@users.noreply.github.com>
2024-10-30 23:05:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'http://localhost:5173','https://assignment-pdt-front.onrender.com/' 
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
 }));
